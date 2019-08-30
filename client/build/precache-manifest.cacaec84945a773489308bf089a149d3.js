fffc1c10ac3b5a2427991bdf93d9453cda30ed03
Ali Sarkeshik <alisarkeshik@gmail.com>
2019-08-30 13:40:32 -0700

    cleaned up some pages

---
+++ client/build/precache-manifest.cacaec84945a773489308bf089a149d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15ac7e98ed38669ea08b15d845428f2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fbc3ccaf157a4e9c8e2",
+    "url": "/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "a75e26867d88780eebd8",
+    "url": "/static/css/main.39cce0cb.chunk.css"
+  },
+  {
+    "revision": "7fbc3ccaf157a4e9c8e2",
+    "url": "/static/js/2.263930ac.chunk.js"
+  },
+  {
+    "revision": "a75e26867d88780eebd8",
+    "url": "/static/js/main.4e849ba2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3c6cd5bb7d95f8526d9ba60173bd6c0a",
+    "url": "/static/media/logo.3c6cd5bb.png"
+  },
+  {
+    "revision": "940687100733d2768316bea97f328250",
+    "url": "/static/media/students.94068710.png"
+  }
+]);
